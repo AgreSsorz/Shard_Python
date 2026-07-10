eaf797cc30ac7a96cbac19e1f416cfca628bc5cc
Agre_Ssor <ireon@n.168.0.11>
2026-07-10 16:09:16 +0700

    Создал главу про Словари

---
+++ Book/Part_6/Practice/1_dicts.py
@@ -0,0 +1,32 @@
+# Человек: использовать словарь для хранения информации об известном человеке.
+# Сохранить имя, фамилию, возраст и город, в котором он живёт.
+# Вывести каждый элемент словаря.
+human = {
+	'first_name': 'howard',
+	'last_name': 'lovecraft',
+	'age': 46,
+	'city': 'providence'
+}
+
+print(f"First_name: {human['first_name']}")
+print(f"Last_name: {human['last_name']}")
+print(f"Age: {human['age']}")
+print(f"City: {human['city']}")
+print()
+
+# Любимые числа: использовать словарь для хранения любимых чисел.
+# Использовать пять имён как ключи для словаря.
+# Вывести имя каждого человека и его любимое число.
+fav_nums = {
+	'denis': 16,
+	'caroline': 47,
+	'daniel': 9,
+	'oleg': 13,
+	'ivan': 5
+	}
+
+print(f"Favorite number of Denis: {fav_nums['denis']}.")
+print(f"Favorite number of Caroline: {fav_nums['caroline']}.")
+print(f"Favorite number of Daniel: {fav_nums['daniel']}.")
+print(f"Favorite number of Oleg: {fav_nums['oleg']}.")
+print(f"Favorite number of Ivan: {fav_nums['ivan']}.")
